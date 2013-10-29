d25966910e087269d84409ebf62205b62366174a
Meekaaeel Muhammad <mamuhammad@fullsail.edu>
2013-10-29 08:24:07 -0400

    recently added
    index.php corrected from terminal

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title> BDF - Meekaaeel Muhammad</title>
+    <link rel="stylesheet"  href="main.css">
+</head>
+<body>
+    <div class="wrapper">
+    	<h1>Meekaaeel Muhammad</h1>
+
+    </div>
+</body>
+</html>
